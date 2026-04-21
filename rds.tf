e3fcd002d837cc2d1aef49c8a2139c404149a516
lauragzzg <lauracgzzg@gmail.com>
2026-04-21 17:48:56 -0400

    Creación de Instancia de MySQL

--- rds.tf
+++ rds.tf
@@ -1,25 +1,15 @@
-resource "aws_db_subnet_group" "rds_private_group" {
-  name       = "losenfasis-rds-private-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-
-  tags = {
-    Name = "Los Enfasis RDS Private Subnet Group"
-  }
-}
-
 resource "aws_db_instance" "BasedeDatos" {
+  identifier             = "${var.basededatos}-mysql"
   allocated_storage      = 20
   engine                 = "mysql"
+  engine_version         = "8.0"
   instance_class         = "db.t3.micro"
-  db_name                = "Basededatos"
-  username               = "Losenfasis"
+  db_name                = var.db_name
+  username               = var.db_username
   password               = var.db_password
-  publicly_accessible    = false
-  db_subnet_group_name   = aws_db_subnet_group.rds_private_group.name
+  db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  publicly_accessible    = false
   skip_final_snapshot    = true
-
-  tags = {
-    Name = "LosEnfasis-DB-Privada"
-  }
+  deletion_protection    = false
 }
